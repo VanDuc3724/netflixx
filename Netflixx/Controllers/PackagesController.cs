488c4f9df50c0d524b6dda6e8761d10b63a31859
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-06-22 08:57:49 +0700

    Add package edit functionality

--- Netflixx/Controllers/PackagesController.cs
+++ Netflixx/Controllers/PackagesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Netflixx.Models;
 using Netflixx.Repositories;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Netflixx.Controllers
@@ -49,6 +50,59 @@ namespace Netflixx.Controllers
             await _context.SaveChangesAsync();
         }
         return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var pkg = await _context.Packages.FindAsync(id);
+        if (pkg == null)
+        {
+            return NotFound();
+        }
+
+        return View(pkg);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, PackagesModel model)
+    {
+        if (id != model.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PackageExists(model.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        return View(model);
+    }
+
+    private bool PackageExists(int id)
+    {
+        return _context.Packages.Any(e => e.Id == id);
     }
 }
 }
