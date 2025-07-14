36be55bf2c7bba3b96d9736bc925a2a0f14123e5
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-14 13:42:36 +0700

    ok

---
+++ Netflixx/Services/Vnpay/IVnPayService.cs
@@ -0,0 +1,11 @@
+﻿using Netflixx.Models.Vnpay;
+
+namespace Netflixx.Services.Vnpay
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+
+    }
+}
