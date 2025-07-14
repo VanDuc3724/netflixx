3e6ba1e6dee0ab4bd858a16726998c9e36423ae4
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-14 23:35:32 +0700

    ok

--- Netflixx/Controllers/FilmpackageController.cs
+++ Netflixx/Controllers/FilmpackageController.cs
@@ -1,43 +1,18 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using Netflixx.Models;
-using Netflixx.Repositories;
 
 namespace Netflixx.Controllers
 {
     public class FilmpackageController : Controller
     {
-        private readonly DBContext _context;
-        private readonly UserManager<AppUserModel> _userManager;
-
-        public FilmpackageController(DBContext context, UserManager<AppUserModel> userManager)
-        {
-            _context = context;
-            _userManager = userManager;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        [Authorize]
-        public async Task<IActionResult> Billhistory()
+        public IActionResult Billhistory()
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return RedirectToAction("Login", "Login");
-            }
-
-            var transactions = await _context.PaymentTransactions
-                .Where(t => t.UserID == user.Id)
-                .OrderByDescending(t => t.TransactionDate)
-                .ToListAsync();
-
-            return View(transactions);
+            return View();
         }
 
         public IActionResult Buy(string packageId, string packageName, int packagePrice)
@@ -51,5 +26,6 @@ namespace Netflixx.Controllers
 
             return View(model);
         }
+
     }
 }
