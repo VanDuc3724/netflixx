003ea306b7fc68afee7b3d0531c97d0e36b49062
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-14 23:21:40 +0700

    ok

--- Netflixx/Controllers/FilmpackageController.cs
+++ Netflixx/Controllers/FilmpackageController.cs
@@ -1,40 +1,20 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using Netflixx.Models;
-using Netflixx.Repositories;
 
 namespace Netflixx.Controllers
 {
-    [Authorize]
     public class FilmpackageController : Controller
     {
-        private readonly UserManager<AppUserModel> _userManager;
-        private readonly DBContext _context;
-
-        public FilmpackageController(UserManager<AppUserModel> userManager, DBContext context)
-        {
-            _userManager = userManager;
-            _context = context;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public async Task<IActionResult> Billhistory()
+        public IActionResult Billhistory()
         {
-            var user = await _userManager.GetUserAsync(User);
-            var history = await _context.PointsTransactions
-                .Where(p => p.UserID == user.Id)
-                .OrderByDescending(p => p.TransactionDate)
-                .ToListAsync();
-            return View(history);
+            return View();
         }
 
-        [HttpGet]
         public IActionResult Buy(string packageId, string packageName, int packagePrice)
         {
             var model = new FilmPackageViewModel
@@ -45,35 +25,6 @@ namespace Netflixx.Controllers
             };
 
             return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Buy(FilmPackageViewModel model)
-        {
-            var user = await _userManager.GetUserAsync(User);
-            var account = await _context.UserAccounts.FirstOrDefaultAsync(a => a.UserID == user.Id);
-
-            if (account == null || account.PointsBalance < model.PackagePrice)
-            {
-                ModelState.AddModelError(string.Empty, "Không đủ coin để thanh toán.");
-                return View(model);
-            }
-
-            account.PointsBalance -= model.PackagePrice;
-
-            _context.PointsTransactions.Add(new PointsTransactionsModel
-            {
-                UserID = user.Id,
-                TransactionDate = DateTime.UtcNow,
-                PointsChange = -model.PackagePrice,
-                Reason = $"Mua gói {model.PackageName}"
-            });
-
-            await _context.SaveChangesAsync();
-
-            TempData["SuccessMessage"] = "Thanh toán thành công";
-            return RedirectToAction(nameof(Billhistory));
         }
 
     }
