fd02d86f83904e9792118aec0a60eb09b887847b
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-15 10:51:59 +0700

    Add manager package creation and access control

---
+++ Netflixx/Areas/Manager/Controllers/PackagesController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Netflixx.Models;
+using Netflixx.Repositories;
+
+namespace Netflixx.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    [Authorize(Roles = "Manager")]
+    public class PackagesController : Controller
+    {
+        private readonly DBContext _context;
+        public PackagesController(DBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var packages = await _context.Packages
+                .Include(p => p.PackageFilms)
+                .ThenInclude(pf => pf.Film)
+                .ToListAsync();
+            return View(packages);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Films = await _context.Films.ToListAsync();
+            return View(new PackagesModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PackagesModel package, int[] selectedFilms)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Packages.Add(package);
+                await _context.SaveChangesAsync();
+
+                if (selectedFilms != null && selectedFilms.Length > 0)
+                {
+                    foreach (var filmId in selectedFilms)
+                    {
+                        _context.PackageFilms.Add(new PackageFilmsModel
+                        {
+                            PackageID = package.Id,
+                            FilmID = filmId
+                        });
+                    }
+                    await _context.SaveChangesAsync();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Films = await _context.Films.ToListAsync();
+            return View(package);
+        }
+    }
+}
