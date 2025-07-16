da0739fe0d7e2d8464ed8c29cb45f812b3c263fa
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-17 02:58:58 +0700

    ok

---
+++ Netflixx/Controllers/PackagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Netflixx.Controllers
+{
+    public class PackagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
