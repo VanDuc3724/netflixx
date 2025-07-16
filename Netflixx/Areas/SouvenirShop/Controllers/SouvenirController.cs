b52c814a0be1ea3136656b7c62e61292dc694a2e
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-17 02:19:57 +0700

    ok

---
+++ Netflixx/Areas/SouvenirShop/Controllers/SouvenirController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Netflixx.Models.Souvenir;
+using Netflixx.Repositories;
+
+[Area("SouvenirShop")]
+public class SouvenirController : Controller
+{
+    private readonly DBContext _context;
+    public SouvenirController(DBContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        List<ProductSouModel> souvenirItems  = _context.ProductSous.ToList();
+        return View(souvenirItems);
+    }
+
+    public IActionResult Detail(int id)
+    {
+        ProductSouModel souvenir = _context.ProductSous.FirstOrDefault(i => i.Id == id);
+        if (souvenir == null) return RedirectToAction("Index");
+        return View(souvenir);
+    }
+
+    [HttpPost]
+    public IActionResult Buy(int id)
+    {
+        // Implement your purchase logic here
+        // For now, we'll just redirect to detail
+        return RedirectToAction("Detail", new { id });
+    }
+}
+
