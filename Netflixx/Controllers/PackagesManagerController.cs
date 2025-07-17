7dc7a55388058c651c1658a1759cab31b3565b52
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-17 07:40:54 +0700

    Add PackagesManager controller and views

---
+++ Netflixx/Controllers/PackagesManagerController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Netflixx.Models;
+using Netflixx.Models.ViewModel;
+using Netflixx.Repositories;
+
+namespace Netflixx.Controllers
+{
+    public class PackagesManagerController : Controller
+    {
+        private readonly DBContext _context;
+
+        public PackagesManagerController(DBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var packages = await _context.Packages
+                .Include(p => p.PackageFilms)
+                .OrderBy(p => p.Name)
+                .ToListAsync();
+            return View(packages);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var vm = new CreatePackageViewModel
+            {
+                Package = new PackagesModel(),
+                Films = await _context.Films.OrderBy(f => f.Title).ToListAsync()
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreatePackageViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Films = await _context.Films.OrderBy(f => f.Title).ToListAsync();
+                return View(model);
+            }
+
+            _context.Packages.Add(model.Package);
+            await _context.SaveChangesAsync();
+
+            if (model.SelectedFilmIds != null && model.SelectedFilmIds.Any())
+            {
+                foreach (var filmId in model.SelectedFilmIds)
+                {
+                    _context.PackageFilms.Add(new PackageFilmsModel
+                    {
+                        PackageID = model.Package.Id,
+                        FilmID = filmId
+                    });
+                }
+                await _context.SaveChangesAsync();
+            }
+
+            TempData["success"] = "Tạo gói phim thành công!";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
