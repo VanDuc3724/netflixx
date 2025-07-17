65f087bef8f607d394a2950ab1c44fba7af92d65
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-17 08:47:37 +0700

    ok

---
+++ Netflixx/Models/PromotionFilmsModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Netflixx.Models
+{
+    public class PromotionFilmsModel
+    {
+        [ForeignKey("Promotion")]
+        public int PromotionID { get; set; }
+        public PromotionsModel Promotion { get; set; }
+
+        [ForeignKey("Film")]
+        public int FilmID { get; set; }
+        public FilmsModel Film { get; set; }
+    }
+}
---
+++ Netflixx/Models/PromotionPackagesModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Netflixx.Models
+{
+    public class PromotionPackagesModel
+    {
+        [ForeignKey("Promotion")]
+        public int PromotionID { get; set; }
+        public PromotionsModel Promotion { get; set; }
+
+        [ForeignKey("Package")]
+        public int PackageID { get; set; }
+        public PackagesModel Package { get; set; }
+    }
+}
