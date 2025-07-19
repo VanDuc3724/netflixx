acb3d3ad2ec07ceb779dfa064bb87f9f89e6ea6c
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-07-19 14:04:36 +0700

    Move package management to PackagesManager

--- Netflixx/Controllers/PackagesManagerController.cs
+++ Netflixx/Controllers/PackagesManagerController.cs
@@ -63,5 +63,94 @@ namespace Netflixx.Controllers
             TempData["success"] = "Tạo gói phim thành công!";
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var package = await _context.Packages
+                .Include(p => p.PackageFilms)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new CreatePackageViewModel
+            {
+                Package = package,
+                Films = await _context.Films.OrderBy(f => f.Title).ToListAsync(),
+                SelectedFilmIds = package.PackageFilms.Select(pf => pf.FilmID).ToList()
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CreatePackageViewModel model)
+        {
+            if (id != model.Package.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                model.Films = await _context.Films.OrderBy(f => f.Title).ToListAsync();
+                return View(model);
+            }
+
+            var package = await _context.Packages
+                .Include(p => p.PackageFilms)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            package.Name = model.Package.Name;
+            package.Description = model.Package.Description;
+            package.Price = model.Package.Price;
+
+            var selectedIds = model.SelectedFilmIds ?? new List<int>();
+            var existingIds = package.PackageFilms.Select(pf => pf.FilmID).ToList();
+
+            var toRemove = package.PackageFilms.Where(pf => !selectedIds.Contains(pf.FilmID)).ToList();
+            _context.PackageFilms.RemoveRange(toRemove);
+
+            var toAdd = selectedIds.Where(fid => !existingIds.Contains(fid))
+                .Select(fid => new PackageFilmsModel { PackageID = package.Id, FilmID = fid });
+            await _context.PackageFilms.AddRangeAsync(toAdd);
+
+            await _context.SaveChangesAsync();
+
+            TempData["success"] = "Cập nhật gói phim thành công!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var package = await _context.Packages.FirstOrDefaultAsync(p => p.Id == id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+            return View(package);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var package = await _context.Packages
+                .Include(p => p.PackageFilms)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (package != null)
+            {
+                _context.PackageFilms.RemoveRange(package.PackageFilms);
+                _context.Packages.Remove(package);
+                await _context.SaveChangesAsync();
+                TempData["success"] = $"Đã xóa gói '{package.Name}'";
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
